8c9bb5b698bedb004f714006cbeda81b792d8778
keguchi <keguchi0413@gmail.com>
2014-03-05 23:34:41 +0900

    Finish chapter 8

---
+++ spec/support/custom_method.rb
@@ -0,0 +1,7 @@
+module CustomMethod
+  def valid_signin(user)
+    fill_in "Email",    with: user.email.upcase
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,8 @@
 include ApplicationHelper
+include CustomMethod
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email.upcase
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
